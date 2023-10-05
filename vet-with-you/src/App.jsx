777500ecc8e67a09509d80622a0d2aec5ca76f5d
Mauricio Emmanuel Resendiz Hurtado <mauricioemmanuel97@gmail.com>
2023-10-04 21:09:24 -0600

    Cambios menores de formato
    se le da un mejor formato al código

--- vet-with-you/src/App.jsx
+++ vet-with-you/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <Landing />
       <Footer />
-      {/* <CounterApp value={99}/> */}
     </div>
   );
 }
--- vet-with-you/src/main.jsx
+++ vet-with-you/src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/MainStyle.css";
-// Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
